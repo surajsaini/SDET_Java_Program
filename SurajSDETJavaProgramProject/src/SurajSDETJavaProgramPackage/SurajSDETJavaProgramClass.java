ead971cc640e7aab560f295fe02bcf574f2a35e4
Suraj Saini <s2008saini@gmail.com>
2024-10-23 12:13:51 -0700

    23-10-2024_FirstCommit

---
+++ SurajSDETJavaProgramProject/src/SurajSDETJavaProgramPackage/SurajSDETJavaProgramClass.java
@@ -0,0 +1,12 @@
+package SurajSDETJavaProgramPackage;
+
+public class SurajSDETJavaProgramClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("SurajSDET");
+
+	}
+
+}
